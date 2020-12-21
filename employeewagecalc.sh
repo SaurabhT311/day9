0b2b9037eaa9fcae86fd031ef6b1171c80a02e53
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2020-12-21 20:30:45 +0530

    Update employeewagecalc.sh

--- employeewagecalc.sh
+++ employeewagecalc.sh
@@ -3,8 +3,7 @@ randomCheck=$((RANDOM%3))
 isFullTime=1
 isPartTime=2
 salary=0
-ratePerHr=20
-
+RatePerHr=20
 
 if [ $isFullTime -eq $randomCheck ]
 then
@@ -16,5 +15,5 @@ else
         numOfworkingHrs=0
 fi
 
-salary=$(( $ratePerHr*$numOfworkingHrs ))
-echo "Employee wage:" $salar
+Salary=$(( $RatePerHr*$numOfworkingHrs ))
+echo "Employee wage:" $Salary
