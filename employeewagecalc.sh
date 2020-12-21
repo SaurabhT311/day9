08efdeb055e9f5e2efeeeaf94f466facbeab7735
SaurabhT311 <38470650+SaurabhT311@users.noreply.github.com>
2020-12-21 20:25:20 +0530

    Update employeewagecalc.sh

--- employeewagecalc.sh
+++ employeewagecalc.sh
@@ -5,39 +5,37 @@ IS_FULL_TIME=1
 IS_PART_TIME=2
 salary=0
 RATE_PER_HR=20
-MAX_WORKING_DAYS=20;
-MAX_WORKING_HRS=60;
+MAX_WORK_DAYS=20;
+MAX_WORK_HRS=60;
 
 #VARIABLES
-totalWorkingDays=1;
-totalWorkingHrs=0;
+TotalworkingDays=1;
+TotalworkingHrs=0;
 
-function getWorkingHrs()
+function getWorkHrs()
 {
         case $1 in
                 $IS_FULL_TIME)
-                empHrs=8
+                        empHrs=8
                 ;;
                 $IS_PART_TIME)
-
-                empHrs=4
+                        empHrs=4
                 ;;
                 *)
-
-                empHrs=0
+                        empHrs=0
                 ;;
         esac
         echo $empHrs
 }
 
-while [[ $totalWorkingHrs -lt $MAX_WORKING_HRS && $totalWorkingDays -lt $MAX_WORKING_DAYS ]]
+while [[ $TotalworkingHrs -lt $MAX_WORK_HRS && $TotalworkingDays -lt $MAX_WORK_DAYS ]]
 do
-        ((totalWorkingDays++))
+        ((TotalworkingDays++))
         empCheck=$((RANDOM%3))
-        empHrs="$getWorkingHrs $empCheck "
-        totalWorkingHrs=$(( $totalWorkingHrs+$empHrs ))
+        empHrs="$getWorkHrs $empCheck "
+        TotalworkingHrs=$(( $TotalworkingHrs+$empHrs ))
 
 done
-totalSalary=$(($totalWorkingHrs*$RATE_PER_HR ))
+TotalSalary=$(($TotalworkingHrs*$RATE_PER_HR ))
 
-echo "Employee wage per month:" $totalSalary
+echo "Employee wage per month:" $TotalSalary
